2e689955d68b530e66ab72d7d96d933d6aeb457c
Michel Ngne <michelngne@gmail.com>
2025-10-07 19:22:59 -0300

    2e commit

--- activite4.py
+++ activite4.py
@@ -9,21 +9,18 @@ Date: 2025-06-06
 
 """
 
-#lire le fichier .env
+# lire le fichier .env
 
 from dotenv import load_dotenv
 import os
 
-#importation de sqlite3
+# importation de sqlite3
 import sqlite3
 import tkinter as tk
 from tkinter import messagebox
 import hashlib
 
-
 load_dotenv()
-
-
 
 nom = os.getenv("NOM")
 motdepasse = os.getenv("MOTDEPASSE")
@@ -31,47 +28,49 @@ db = os.getenv("DB")
 port = os.getenv("PORT")
 host = os.getenv("HOST")
 
-#print(f"Nom: {nom}, Mot de passe: {motdepasse}, DB: {db}, Port: {port}, Host: {host}")
+# print(f"Nom: {nom}, Mot de passe: {motdepasse}, DB: {db}, Port: {port}, Host: {host}")
 
-# UI - Login 
+# UI - Login
+
 def save_user(username, password):
-        # SQL (SAVE AU DB)
-        # global c, conn
-        conn = sqlite3.connect(db)
-        c = conn.cursor()
-        c.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL UNIQUE,
-            password TEXT
-        )
-    ''')
-        
-        password = hashlib.sha256(password.encode()).hexdigest()
-        
-        c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
-        conn.commit()
-        conn.close()
-        
-        print("SAVE")
+    # SQL (SAVE AU DB)
+    password = hashlib.sha256(password.encode()).hexdigest()
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                password TEXT
+            )
+        ''')
+
+    c.execute('INSERT INTO users (username, password) VALUES (?, ?)',
+            (username, password))
+    conn.commit()
+    conn.close()
+
+    print("SAVE")
 
 
-def validate_user(username, password): 
-        # VERIFIER AVEC SQL (OUBLIER PAS LE HASH)
-        conn = sqlite3.connect(db)
-        c = conn.cursor()
-        password = hashlib.sha256(password.encode()).hexdigest()
-        c.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
-        result = c.fetchone()
-        return result is not None
-    
-#Login dans l'interface
+def validate_user(username, password):
+    # VERIFIER AVEC SQL (OUBLIER PAS LE HASH)
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    password = hashlib.sha256(password.encode()).hexdigest()
+    c.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+    result = c.fetchone()
+    return result is not None
+
+# Login dans l'interface
+
 def login():
     if validate_user(username_entry.get(), password_entry.get()):
         messagebox.showinfo("Login", "CONENXION REUSSIE")
     else:
         messagebox.showinfo("Login", "ECHEC DE CONENXION")
-        
+
+
 def open_register():
     print("NOUVELLE FENETRE POUR NOUVEL UTILISATEUR")
 
@@ -79,16 +78,15 @@ def open_register():
     register_window.title("Nouvel Utilisateur")
     register_window.geometry("300x200")
     tk.Label(register_window, text="Nom User").pack()
-    new_username_entry = tk.Entry(register_window)  
+    new_username_entry = tk.Entry(register_window)
     new_username_entry.pack()
-    
-    tk.Label(register_window, text="Mot de passe").pack()
-    new_password_entry = tk.Entry(register_window, show="*") 
-    new_password_entry.pack()
-    tk.Button(register_window, text="Enregistrer", command=lambda: save_user(new_username_entry.get(), new_password_entry.get())).pack(pady=5)
-    
 
-    
+    tk.Label(register_window, text="Mot de passe").pack()
+    new_password_entry = tk.Entry(register_window, show="*")
+    new_password_entry.pack()
+    tk.Button(register_window, text="Enregistrer", command=lambda: save_user(
+        new_username_entry.get(), new_password_entry.get())).pack(pady=5)
+
 
 window = tk.Tk()
 window.title("Authentification")
---
+++ dist/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-07T19:16:51.054522
+from .pyarmor_runtime import __pyarmor__
